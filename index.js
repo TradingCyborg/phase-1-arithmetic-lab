f1b0cb2b934ca7bde3ebf19b2bf67052039efb60
Daniel Kinyua <my.github.kinyuad29@gmail.com>
2023-09-20 21:07:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+console.log(multiply);
+var random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+var num3 = 20;  // You can choose any value for num3
+var num4 = 4;   // You can choose any value for num4
 
-// Write your code here
+var mod = (num3 % num4) + 4;  // Calculate the remainder and add 4 to it
+
+console.log(mod);  // This will print 8 to the console
+var numbers = [5, 10, 20, 15, 7];  // Define an array of numbers
+
+var max = Math.max(...numbers);  // Use Math.max() with the spread operator to find the maximum
+
+console.log(max);  // This will print 20 to the console
